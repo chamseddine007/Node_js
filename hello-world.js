cae24659cf3ca0d715bf8f2f0e8a738eac4903b2
chamseddine007 <trikichams6@gmail.com>
2023-07-11 14:26:23 +0100

    first commit

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+var http =require("http")
+
+
+http.createServer(function(req,res){res.end("<h1>Hello Node!!!!</h1>\n")}).listen(3000)
+console.log("Server is runnig")
+const fs = require('fs');
+
+
+
+
